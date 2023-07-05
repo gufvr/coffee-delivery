ebdcc2d164d9b103863478407caf21de1516c62b
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-07-05 16:44:37 -0300

    creating the Intro component

---
+++ src/pages/Home/components/Intro/index.tsx
@@ -0,0 +1,11 @@
+import { IntroContainer, IntroContent } from "./styles";
+
+export function Intro() {
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <p>teste</p>
+      </IntroContent>
+    </IntroContainer>
+  )
+}
