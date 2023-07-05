55d8f4e5f1d240e8f32344ed516423718362bad9
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-07-05 15:22:03 -0300

    doing the setup to develop the Header component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/Home";
+import { CompleteOrderPage } from "./pages/CompleteOrder";
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<DefaultLayout />}>
       <Route path='/' element={<HomePage />} />
+      <Route path='/completeOrder' element={<CompleteOrderPage />} />
+      </Route>
     </Routes>
   )
 }
