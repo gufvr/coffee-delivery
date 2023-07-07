e9230cb5323c3232ed2c9a70d7360f12ceeb4bdd
Gustavo Favero <gustavo.vfavero@gmail.com>
2023-07-06 22:39:22 -0300

    creating the CompleteOrderForm

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,26 @@
 import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styles";
 import coffeeLogoImg from '../../assets/coffee-delivery-logo.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react'
+import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
     <HeaderContainer>
       <div className="container">
+        <NavLink to="/">
         <img src={coffeeLogoImg} alt="" />
+        </NavLink>
 
         <HeaderButtonsContainer>
           <HeaderButton variant="purple">
             <MapPin size={20} weight='fill' />
             Sorocaba, SP
           </HeaderButton>
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight='fill' />
-          </HeaderButton>
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              <ShoppingCart size={20} weight='fill' />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
